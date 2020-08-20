a6c20f1ce850f7470b791fe7e458bfc480c34bf9
Midas Lambrichts <midaslamb@gmail.com>
2020-08-20 20:14:28 +0200

    Initial commit

---
+++ src/data.rs
@@ -0,0 +1,350 @@
+use chrono::NaiveDate;
+
+pub(crate) struct Me {
+    pub(crate) first_name: &'static str,
+    pub(crate) last_name: &'static str,
+    pub(crate) location: &'static str,
+    pub(crate) birth_date: NaiveDate,
+    pub(crate) contact_email: &'static str,
+    pub(crate) links: Vec<PersonLink>,
+    pub(crate) scholary_experiences: Vec<ScholaryExperience>,
+    pub(crate) work_experiences: Vec<WorkExperience>,
+    pub(crate) languages: Vec<Language>,
+    pub(crate) skills: Vec<Skill>,
+    pub(crate) projects: Vec<Project>,
+}
+
+impl Default for Me {
+    fn default() -> Self {
+        let mut me = Me {
+            first_name: "Midas",
+            last_name: "Lambrichts",
+            location: "Turnhout",
+            birth_date: NaiveDate::from_ymd(1996, 02, 06),
+            contact_email: "midaslamb@gmail.com",
+            links: PersonLink::get_all(),
+            scholary_experiences: ScholaryExperience::get_all(),
+            work_experiences: WorkExperience::get_all(),
+            languages: Language::get_all(),
+            skills: Skill::get_all(),
+            projects: Project::get_all(),
+        };
+
+        me.scholary_experiences.sort_by_key(|k| k.start_date);
+        me.scholary_experiences.reverse();
+
+        me.work_experiences.sort_by_key(|k| k.start_date);
+        me.work_experiences.reverse();
+
+        me.projects.sort_by_key(|k| k.priority);
+        me.projects.reverse();
+
+        me
+    }
+}
+
+pub(crate) enum PersonLink {
+    GitHub { username: &'static str },
+    LinkedIn { link_name: &'static str },
+}
+
+impl PersonLink {
+    fn get_all() -> Vec<Self> {
+        use PersonLink::*;
+        vec![
+            GitHub {
+                username: "MidasLamb",
+            },
+            LinkedIn {
+                link_name: "midas-lambrichts",
+            },
+        ]
+    }
+
+    pub(crate) fn linkify(&self) -> String {
+        match self {
+            PersonLink::GitHub { username: u } => format!("https://github.com/{}/", u),
+            PersonLink::LinkedIn { link_name: l } => format!("https://www.linkedin.com/in/{}/", l),
+        }
+    }
+
+    pub(crate) fn short_view(&self) -> String {
+        match self {
+            PersonLink::GitHub { username: u } => format!("/{}/", u),
+            PersonLink::LinkedIn { link_name: l } => format!("/in/{}/", l),
+        }
+    }
+}
+
+pub(crate) struct ScholaryExperience {
+    pub(crate) start_date: NaiveDate,
+    pub(crate) end_date: Option<NaiveDate>,
+    pub(crate) institution: &'static str, // TODO: Turn into enum?
+    pub(crate) degree: &'static str,      // TODO: Turn into enum?
+    pub(crate) honors: Option<&'static str>, // TODO: Turn into enum?
+    pub(crate) thesis_title: Option<&'static str>,
+}
+
+impl ScholaryExperience {
+    fn get_all() -> Vec<Self> {
+        vec![ScholaryExperience {
+            start_date: NaiveDate::from_ymd(2014, 9, 25),
+            end_date: Some(NaiveDate::from_ymd(2017, 6, 30)),
+            institution: "KU Leuven",
+            degree: "Bachelor of informatics",
+            honors: Some("Cum Laude"),
+            thesis_title: None,
+        },ScholaryExperience {
+            start_date: NaiveDate::from_ymd(2017, 9, 25),
+            end_date: Some(NaiveDate::from_ymd(2019, 6, 30)),
+            institution: "KU Leuven",
+            degree: "Master Computer Science",
+            honors: Some("Cum Laude"),
+            thesis_title: Some(
+                "The Effects of the position of the Tutorial Persona throughout the Rose of Leary.",
+            ),
+        },
+        ]
+    }
+}
+
+pub(crate) struct WorkExperience {
+    pub(crate) start_date: NaiveDate,
+    pub(crate) end_date: Option<NaiveDate>,
+    pub(crate) title: &'static str,
+    pub(crate) company: &'static str,
+    pub(crate) summary: &'static str,
+}
+
+impl WorkExperience {
+    fn get_all() -> Vec<Self> {
+        vec![
+            WorkExperience {
+                start_date: NaiveDate::from_ymd(2015, 01, 01),
+                end_date: Some(NaiveDate::from_ymd(2016, 12, 31)),
+                title: "Software Tester",
+                company: "Bringme",
+                summary: r"Responsible for manually testing (including regression tests) of mobile and web applications.
+Found a lot of edge-cases, which made me the least favorite tester of the developers.
+Bringme introduced me to agile development.",
+            },
+            WorkExperience {
+                start_date: NaiveDate::from_ymd(2017, 07, 20),
+                end_date: Some(NaiveDate::from_ymd(2018, 06, 30)),
+                title: "Member of Marketing Committee",
+                company: "Ekonomika VZW",
+                summary: r"Responsible for the entire IT-infrastructure of Ekonomika VZW, which included, but was not
+limited to: creating new sites, maintaining sites, domain administration, transferring email setup
+to GSuite, introducing Git (and GitHub) for version control,...",
+            },
+            WorkExperience {
+                start_date: NaiveDate::from_ymd(2019, 07, 15),
+                end_date: None,
+                title: "Software Engineer",
+                company: "Dover Fueling Solutions",
+                summary: r"Responsible for moving data management from C++ to C# in a Point of Sale (POS) system,
+and ensuring that in less-than-ideal network conditions, the data is synchronised with the Cloud. Other responsibilities include 
+maintaining a Web Server, and investigating improvements. My team worked in an Agile fashion (Scrum), with two-week sprints.",
+            },
+        ]
+    }
+}
+
+pub(crate) struct Language {
+    pub(crate) language: &'static str,
+    pub(crate) proficiency: LanguageProficiency,
+}
+
+impl Language {
+    pub(crate) fn get_all() -> Vec<Self> {
+        vec![
+            Language {
+                language: "English",
+                proficiency: LanguageProficiency::Proficient,
+            },
+            Language {
+                language: "Dutch",
+                proficiency: LanguageProficiency::NativeSpeaker,
+            },
+            Language {
+                language: "French",
+                proficiency: LanguageProficiency::Basic,
+            },
+            Language {
+                language: "German",
+                proficiency: LanguageProficiency::Basic,
+            },
+        ]
+    }
+}
+
+pub(crate) enum LanguageProficiency {
+    NativeSpeaker,
+    Proficient,
+    Basic,
+}
+
+impl std::string::ToString for LanguageProficiency {
+    fn to_string(&self) -> String {
+        let v = match self {
+            LanguageProficiency::NativeSpeaker => "Native Speaker",
+            LanguageProficiency::Proficient => "Proficient",
+            LanguageProficiency::Basic => "Basic knowledge",
+        };
+        v.to_owned()
+    }
+}
+
+pub(crate) struct Skill {
+    pub(crate) name: &'static str,
+    pub(crate) ability: u8,
+    pub(crate) note: Option<SkillNote>,
+}
+
+impl Skill {
+    pub(crate) fn get_all() -> Vec<Skill> {
+        vec![
+            Skill {
+                name: "C#",
+                ability: 8,
+                note: Some(SkillNote::AutoDidact),
+            },
+            Skill {
+                name: "Rust",
+                ability: 6,
+                note: Some(SkillNote::AutoDidact),
+            },
+            Skill {
+                name: "Git",
+                ability: 7,
+                note: None,
+            },
+            Skill {
+                name: "SQL",
+                ability: 7,
+                note: None,
+            },
+            Skill {
+                name: "Typescript",
+                ability: 7,
+                note: Some(SkillNote::AutoDidact),
+            },
+            Skill {
+                name: "Software Architecture",
+                ability: 6,
+                note: None,
+            },
+            Skill {
+                name: "Java",
+                ability: 6,
+                note: None,
+            },
+            Skill {
+                name: "Information Visualisation",
+                ability: 6,
+                note: None,
+            },
+        ]
+    }
+}
+
+pub(crate) enum SkillNote {
+    AutoDidact,
+}
+
+impl std::string::ToString for SkillNote {
+    fn to_string(&self) -> String {
+        let v = match self {
+            SkillNote::AutoDidact => "Autodidact",
+        };
+        v.to_owned()
+    }
+}
+
+pub(crate) struct Project {
+    pub(crate) name: &'static str,
+    pub(crate) used_technologies: Vec<Technology>,
+    pub(crate) link: Option<&'static str>,
+    pub(crate) summary: &'static str,
+    pub(crate) priority: usize,
+}
+
+impl Project {
+    pub(crate) fn get_all() -> Vec<Self> {
+        use Technology::*;
+        vec![
+            Project {
+                name: "KBBC T&T Turnhout",
+                used_technologies: vec![PHP, Laravel, Bootstrap, JavaScript, CSS3, HTML5, VueJs],
+                link: Some("https://github.com/MidasLamb/TTTurnhout"),
+                summary: "A Laravel Application which reads data from Basketbal Vlaanderen, and displays it in a userfriendly manner.
+This is currently being rebuild in a complete Rust stack (async-graphql, actix, yew, graphql-client)",
+                priority: 0
+            },
+            Project {
+                name: "Ekonomika KD",
+                used_technologies: vec![PHP, Laravel, Bootstrap, JavaScript, CSS3, HTML5],
+                link: None,
+                summary: "A Site to enable FEB students of the KU Leuven to reserve books.
+The application handles the picking up of said books, expiration in case students take too long to
+pick up their books, adding stock when a delivery is made, and preventing reservations in case
+stock is too low. Hooks into a legacy systems which handles the monetary part of the
+transactions. Most of the site required a KU Leuven SSO Login.",
+                priority: 1
+            },
+            Project {
+                name: "CS Schedule",
+                used_technologies: vec![PHP, JavaScript, CSS3, HTML5],
+                link: Some("https://cs-schedule.herokuapp.com/"),
+                summary: "A small site which allows students computer science to get the time and place of courses for any
+course of the master they want, which is necessary because some dutch courses require you to
+take the english lectures, but they don't appear in the students their timetable provided by the KU
+Leuven. It parses the courses from a webpage provided by the KU Leuven.",
+                priority: 2,
+            },
+            Project {
+                name: "Curriculum Vitae",
+                used_technologies: vec![Rust, Yew, Wasm, CSS3, HTML5, Sass],
+                link: Some("/#"),
+                summary: r#"A CV is made of up two parts: Data & Representation. "Traditional" ways of making a CV (e.g. Word) do not account
+for these. Data and representation are mixed, which I didn't like. That's why I decided to create my CV as a "webpage", which is then printed to pdf.
+This lets me vary data (e.g. adding a new project) and representation (e.g. CSS), separately, without much hassle.
+This CV is created through with Rust (Yew for building the wasm/html, grass for turning the Sass into CSS)."#,
+                priority: 1
+            }
+        ]
+    }
+}
+
+pub(crate) enum Technology {
+    PHP,
+    Laravel,
+    Bootstrap,
+    JavaScript,
+    HTML5,
+    CSS3,
+    VueJs,
+    Rust,
+    Yew,
+    Wasm,
+    Sass,
+}
+
+impl std::string::ToString for Technology {
+    fn to_string(&self) -> String {
+        let v = match self {
+            Technology::PHP => "PHP",
+            Technology::Laravel => "Laravel",
+            Technology::Bootstrap => "Bootstrap",
+            Technology::JavaScript => "JavaScript",
+            Technology::HTML5 => "HTML5",
+            Technology::CSS3 => "CSS3",
+            Technology::VueJs => "Vue.js",
+            Technology::Rust => "Rust",
+            Technology::Yew => "Yew",
+            Technology::Wasm => "Wasm",
+            Technology::Sass => "Sass",
+        };
+        v.to_owned()
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,290 @@
+#![recursion_limit = "1024"]
+
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+mod data;
+
+use data::Me;
+
+const DATE_FORMAT: &str = "%m/%Y";
+
+trait Htmlify {
+    fn view(&self) -> Html;
+}
+
+struct Model {
+    person: Me,
+}
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+    fn create(_: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Self {
+            person: Me::default(),
+        }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        let contact_email_link = "mailto:".to_string()
+            + self.person.contact_email
+            + "?subject=You're hired!&body=We saw your CV and want to hire you immediately!";
+
+        let person_links = self.person.links.view();
+        let work_experience = self.person.work_experiences.view();
+        let scholary_experience = self.person.scholary_experiences.view();
+        let languages = self.person.languages.view();
+        let skills = self.person.skills.view();
+        let projects = self.person.projects.view();
+
+        html! {
+            <>
+                <div class="page">
+                    <div class="title">
+                        <span class="cv">{"Curriculum Vitae: "}</span><span class="name">{self.person.first_name.to_owned() + " " + self.person.last_name}</span>
+                    </div>
+                    <hr/>
+                    <div class="person-details">
+                        <div class="details">
+                            <table>
+                                <tr>
+                                    <td class="header">{"Full name: "}</td>
+                                    <td class="name">{self.person.first_name.to_owned() + " " + self.person.last_name}</td>
+                                </tr>
+                                <tr>
+                                    <td class="header">{"Email: "}</td>
+                                    <td class="name"><a href=contact_email_link target="_blank">{self.person.contact_email}</a></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="links">
+                            {person_links}
+                        </div>
+                    </div>
+                    <hr/>
+                    <main>
+                        <div class="experience work">
+                            <h2>{"Work Experience"}</h2>
+                            <div class="container">
+                                {work_experience}
+                            </div>
+                        </div>
+                        <hr/>
+                        <div class="experience education">
+                            <h2>{"Education"}</h2>
+                            <div class="container">
+                                {scholary_experience}
+                            </div>
+                        </div>
+                        <hr/>
+                        <div class="languages">
+                            <h2>{"Languages"}</h2>
+                            <div class="container">
+                                {languages}
+                            </div>
+                        </div>
+                        <hr/>
+                        <div class="skills">
+                            <h2>{"Skills"}</h2>
+                            <div class="container">
+                                {skills}
+                            </div>
+                        </div>
+                    </main>
+                </div>
+                <div class="page">
+                    <main>
+                        <div class="projects">
+                            <h2>{"Projects"}</h2>
+                            <div class="container">
+                                {projects}
+                            </div>
+                        </div>
+                    </main>
+                </div>
+            </>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
+}
+
+impl Htmlify for data::PersonLink {
+    fn view(&self) -> Html {
+        let icon = match self {
+            data::PersonLink::GitHub { username } => {
+                html! {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/></svg>}
+            }
+            data::PersonLink::LinkedIn { link_name } => {
+                html! {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z"/></svg>}
+            }
+        };
+
+        html! {
+            <div>
+                <a href=self.linkify()>{icon}{self.short_view()}</a>
+            </div>
+        }
+    }
+}
+
+impl Htmlify for data::WorkExperience {
+    fn view(&self) -> Html {
+        let start_date = format!("{}", self.start_date.format(DATE_FORMAT));
+        let end_date = self.end_date.map_or("Present".to_owned(), |x| {
+            format!("{}", x.format(DATE_FORMAT))
+        });
+        html! {
+            <div class="experience-container">
+                <div class="company">
+                    <span class="header">{"Company: "}</span>
+                    <span class="value">{self.company}</span>
+                </div>
+                <div class="job-title">
+                    <span class="header">{"Job title: "}</span>
+                    <span class="value">{self.title}</span>
+                </div>
+                <div class="summary">
+                    {self.summary}
+                </div>
+                <div class="date">
+                    {start_date}{" - "}{end_date}
+                </div>
+            </div>
+        }
+    }
+}
+
+impl Htmlify for data::ScholaryExperience {
+    fn view(&self) -> Html {
+        let start_date = format!("{}", self.start_date.format(DATE_FORMAT));
+        let end_date = self.end_date.map_or("Present".to_owned(), |x| {
+            format!("{}", x.format(DATE_FORMAT))
+        });
+        html! {
+            <div class="education-container">
+                <div class="degree">
+                    <span class="value">{self.degree}</span>
+                    <span class="honors">
+                        {self.honors.unwrap()}
+                    </span>
+                </div>
+                <div class="institution">
+                    <span class="header">{"Institution: "}</span>
+                    <span class="value">{self.institution}</span>
+                </div>
+                {
+                    if self.thesis_title.is_some() {
+                        html!{
+                            <div class="thesis">
+                                <span class="header">{"Thesis: "}</span>
+                                <span class="value">{self.thesis_title.unwrap()}</span>
+                            </div>
+                        }
+                    } else {
+                        html! {}
+                    }
+                }
+                <div class="date">
+                    {start_date}{" - "}{end_date}
+                </div>
+            </div>
+        }
+    }
+}
+
+impl Htmlify for data::Language {
+    fn view(&self) -> Html {
+        html! {
+            <div class="language">
+                <div class="lang">{self.language}</div>
+                <div class="proficiency">{self.proficiency.to_string()}</div>
+            </div>
+        }
+    }
+}
+
+impl Htmlify for data::Skill {
+    fn view(&self) -> Html {
+        let note = self.note.as_ref().map_or(
+            html! {},
+            |n| html! {<span class="note">{n.to_string()}</span>},
+        );
+        let mut ability = vec![];
+        for i in (0..10).step_by(2) {
+            if i + 1 == self.ability {
+                ability.push(html! {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 536 512"><path d="M508.55 171.51L362.18 150.2 296.77 17.81C290.89 5.98 279.42 0 267.95 0c-11.4 0-22.79 5.9-28.69 17.81l-65.43 132.38-146.38 21.29c-26.25 3.8-36.77 36.09-17.74 54.59l105.89 103-25.06 145.48C86.98 495.33 103.57 512 122.15 512c4.93 0 10-1.17 14.87-3.75l130.95-68.68 130.94 68.7c4.86 2.55 9.92 3.71 14.83 3.71 18.6 0 35.22-16.61 31.66-37.4l-25.03-145.49 105.91-102.98c19.04-18.5 8.52-50.8-17.73-54.6zm-121.74 123.2l-18.12 17.62 4.28 24.88 19.52 113.45-102.13-53.59-22.38-11.74.03-317.19 51.03 103.29 11.18 22.63 25.01 3.64 114.23 16.63-82.65 80.38z"/></svg>});
+            } else if i < self.ability {
+                ability.push(html! {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M259.3 17.8L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0z"/></svg>});
+            } else {
+                ability.push(html! {<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M528.1 171.5L382 150.2 316.7 17.8c-11.7-23.6-45.6-23.9-57.4 0L194 150.2 47.9 171.5c-26.2 3.8-36.7 36.1-17.7 54.6l105.7 103-25 145.5c-4.5 26.3 23.2 46 46.4 33.7L288 439.6l130.7 68.7c23.2 12.2 50.9-7.4 46.4-33.7l-25-145.5 105.7-103c19-18.5 8.5-50.8-17.7-54.6zM388.6 312.3l23.7 138.4L288 385.4l-124.3 65.3 23.7-138.4-100.6-98 139-20.2 62.2-126 62.2 126 139 20.2-100.6 98z"/></svg>});
+            }
+        }
+
+        html! {
+            <div class="skill-container">
+                <div class="skill">
+                    <span class="name">{self.name}</span>
+                    {note}
+                </div>
+                <div class="ability">
+                    {ability}
+                </div>
+            </div>
+        }
+    }
+}
+
+impl Htmlify for data::Project {
+    fn view(&self) -> Html {
+        let technologies = self
+            .used_technologies
+            .iter()
+            .map(|t| t.to_string())
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        html! {
+            <div class="project-container">
+                <div class="project-header">
+                    <div class="project-name">{self.name}</div>
+                    {
+                        if self.link.is_some() {
+                            html!{
+                                <a href=self.link.unwrap() class="project-link" target="_blank">{"Link"}</a>
+                            }
+                        } else {
+                            html! {}
+                        }
+                    }
+                </div>
+                <div class="used-technologies">{technologies}</div>
+                <div class="summary">
+                    {self.summary}
+                </div>
+            </div>
+        }
+    }
+}
+
+impl<T> Htmlify for Vec<T>
+where
+    T: Htmlify,
+{
+    fn view(&self) -> Html {
+        let v = self.iter().map(|x| x.view()).collect::<Vec<_>>();
+        html! {<>{v}</>}
+    }
+}
